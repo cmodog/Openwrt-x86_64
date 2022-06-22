4b6c22a7fc066887342e46309c2b4798ca4e7f87
cmodog <53931809+cmodog@users.noreply.github.com>
2022-06-22 23:36:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 git clone https://github.com/rufengsuixing/luci-app-syncdial.git package/luci-app-syncdial
